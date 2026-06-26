897a052c65a5c8a704241e8bde0f9b1ff9d0d303
MyBook Hype <mybookhype@example.com>
2026-06-26 15:53:16 +0700

    Add new logo SVG file to assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import { imagetools } from 'vite-imagetools';
 
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
@@ -9,7 +10,20 @@ import icon from 'astro-icon';
 export default defineConfig({
   site: 'https://tollmanufacturing.id',
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      imagetools({
+        defaultDirectives: (url) => {
+          if (url.searchParams.has('webp')) {
+            return new URLSearchParams({
+              format: 'webp',
+              w: '800;1200;1600',
+            });
+          }
+          return new URLSearchParams();
+        },
+      }),
+    ],
   },
   output: 'static',
 
